8c4d261293a1b4f5fba34519dce371968f1e2996
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2017-05-15 14:15:25 -0700

    variable gpu id

--- run_BEGAN.sh
+++ run_BEGAN.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-GPUID=1
+GPUID=$1
 
 CUDA_VISIBLE_DEVICES=$GPUID python main.py \
 	--gpuid=$GPUID \
-	--start-epoch=0 \
+	--start-epoch=500 \
 	--add-epochs=500 \
 	--save-every=1 \
 	--gamma=0.75 \
 	--hidden_size=16 \
-	--start-learn-rate=0.0001 \
+	--start-learn-rate=0.001 \
 	--decay-every=-1
